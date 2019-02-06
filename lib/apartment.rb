1aa841790f4d0a4063e701bb42d5464e68dea87b
wipegup <wipegup@gmail.com>
2019-02-06 09:18:54 -0700

    Passing has attributes

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,6 +1,16 @@
 class Apartment
-  def initialize(info_hash)
+  attr_reader :number,
+              :monthly_rent,
+              :bathrooms,
+              :bedrooms,
+              :renter
 
+  def initialize(info_hash)
+    @number = info_hash[:number]
+    @monthly_rent = info_hash[:monthly_rent]
+    @bathrooms = info_hash[:bathrooms]
+    @bedrooms = info_hash[:bedrooms]
+    @renter = nil
   end
 
 end
