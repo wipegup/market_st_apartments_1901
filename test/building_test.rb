28ddfa2d5c1ef279b259cb44045cf4071c4789e8
wipegup <wipegup@gmail.com>
2019-02-06 09:29:24 -0700

    Add apartments to test setup

--- test/building_test.rb
+++ test/building_test.rb
@@ -7,6 +7,14 @@ class BuildingTest < MiniTest::Test
 
   def setup
     @building = Building.new
+    @a1 = Apartment.new({number: "A1",
+                          monthly_rent: 1200,
+                          bathrooms: 1,
+                          bedrooms: 1})
+    @b2 = Apartment.new({number: "B2",
+                          monthly_rent: 999,
+                          bathrooms: 2,
+                          bedrooms: 2})
   end
 
   def test_it_exists
